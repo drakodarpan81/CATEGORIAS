9f7b304f66824a4aad961abfd14b11d252f6a010
drakodarpan81 <drakodarpan@gmail.com>
2023-05-26 12:27:44 -0700

    Second commit

--- Scripts/tb_categorias.sql
+++ Scripts/tb_categorias.sql
@@ -5,7 +5,7 @@ CREATE TABLE tb_categorias(
 	numero_categoria INTEGER NOT NULL,
 	nombre_categoria VARCHAR(50) NOT NULL,
 	folio INTEGER NOT NULL DEFAULT 0,
-	estado SMALLINT NOT NULL DEFAULT 0,
+	status SMALLINT NOT NULL DEFAULT 0,
 	fecha_alta DATE NOT NULL DEFAULT CURRENT_DATE,
 	PRIMARY KEY (numero_categoria, nombre_categoria)
 );
